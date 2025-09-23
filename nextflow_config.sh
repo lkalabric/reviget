078d36f9a80abeb6fd64cbaba4661b0bd882a72e
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-09-23 18:15:34 -0300

    Update nextflow_config.sh

--- nextflow_config.sh
+++ nextflow_config.sh
@@ -3,7 +3,7 @@
 # --- Download e instalação do pacote nextflow ---
 wget -qO nextflow https://github.com/nextflow-io/nextflow/releases/download/v23.10.1/nextflow-23.10.1-all
 chmod +x nextflow
-mv nextflow /usr/local/bin
+mv nextflow /bin
 nextflow info
 
 # --- Atualização para a versão mais nova do pacote Nextflow ---

7f95c3eb61bc78e679bb7ebbb3c97c35391ce01e
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-09-25 20:08:36 -0300

    Update conda_config.sh

--- conda_config.sh
+++ conda_config.sh
@@ -3,3 +3,6 @@
 # --- Instalação do gerenciador Conda no Linux ---
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
+
+ln -s /home/kalabric/miniconda3/bin/conda /usr/bin/conda
+conda --version

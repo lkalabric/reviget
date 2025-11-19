c28739582768fa1782d4b707e489ef094c3c5ee2
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-11-19 09:19:42 -0300

    Update MAGMA_analysis.sh

--- MAGMA_analysis.sh
+++ MAGMA_analysis.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/bash
 
 # --- Pipeline setup sanity check ---
+# Na Fiocruz isso não roda por conta do acesso a internet tanto na rede corporativa quanto na rede wifi (independente da rede)
 nextflow run 'https://github.com/TORCH-Consortium/MAGMA' \
-         -profile test \
-         -r v2.2.2
+         -profile test,docker,laptop \
+         -r v2.2.0
       
 exit 0;
 
-# --- Análise dos dados de teste ---
+# --- Análise dos dados de teste localmente ---
 nextflow run 'https://github.com/TORCH-Consortium/MAGMA' \
-         -profile conda_local, server \
+         -profile docker,laptop \
          -r v2.2.2 \
-         -params-file examples/reviget/my_parameters_test.1sample.yml
+         -params-file examples/regivet/my_parameters_1_local.yml
 
 nextflow run 'https://github.com/TORCH-Consortium/MAGMA' \
-         -profile conda_local, server \
+         -profile docker,laptop \
          -r v2.2.2 \
-         -params-file examples/regivet/my_parameters_1.yml
+         -params-file examples/regivet/my_parameters_3_local.yml
+
+# --- Análise dos dados de teste remotamente ---
+# Na Fiocruz isso não roda por conta do acesso a internet 
+         nextflow run 'https://github.com/TORCH-Consortium/MAGMA' \
+         -profile docker,laptop \
+         -r v2.2.2 \
+         -params-file examples/regivet/my_parameters_3_remote.yml
